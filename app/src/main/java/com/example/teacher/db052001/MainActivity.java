83e33ae61c940011d4a1637e33fd318df69ec16d
Shaojung <shaojung@gmail.com>
2017-05-20 10:48:24 +0800

    BLE

--- app/src/main/java/com/example/teacher/db052001/MainActivity.java
+++ app/src/main/java/com/example/teacher/db052001/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.teacher.db052001;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
+public class MainActivity extends AppCompatActivity {
+    final int REQUEST_CODE = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int permission = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+        if (permission != PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] {Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        }
     }
 }
