f733590be7e66423568b1ed1bc49e71ca1ca3650
Shaojung <shaojung@gmail.com>
2017-05-20 14:12:31 +0800

    BLE

--- app/src/main/java/com/example/teacher/db052001/MainActivity.java
+++ app/src/main/java/com/example/teacher/db052001/MainActivity.java
@@ -1,26 +1,43 @@
 package com.example.teacher.db052001;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class MainActivity extends AppCompatActivity {
     final int REQUEST_CODE = 123;
+    String targetAddr;
+    BluetoothAdapter BTadapter;
+    BluetoothLeScanner scanner;
+    BluetoothDevice BTDevice;
+    MyScanCallBack callBack = new MyScanCallBack();
+    Handler hander;
+    TextView tv1;
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hander = new Handler();
         btn = (Button) findViewById(R.id.button);
+        tv1 = (TextView) findViewById(R.id.textView);
         int permission = ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_COARSE_LOCATION);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -45,7 +62,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Toast.makeText(MainActivity.this, data.getStringExtra("addr"), Toast.LENGTH_LONG).show();
+        if (requestCode == 123 && resultCode == RESULT_OK)
+        {
+            targetAddr = data.getStringExtra("addr");
+            tv1.setText(targetAddr);
+            Toast.makeText(MainActivity.this, data.getStringExtra("addr"), Toast.LENGTH_LONG).show();
+        }
+
     }
 
     @Override
@@ -67,4 +90,49 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+    public void BTScan()
+    {
+        if (BTadapter == null)
+        {
+            Toast.makeText(MainActivity.this, "No Bluetooth", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!BTadapter.isEnabled())
+        {
+            BTadapter.enable();
+        }
+
+        scanner.startScan(callBack);
+        hander.postDelayed(runnable, 10000);
+
+    }
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            scanner.stopScan(callBack);
+            Log.d("BLE1", "Scan stop!!");
+
+        }
+    };
+    class MyScanCallBack extends ScanCallback
+    {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            if (targetAddr.equals(result.getDevice().getAddress()))
+            {
+                BTDevice = result.getDevice();
+                Log.d("BLE1", "Connect!!!") ;
+            }
+
+        }
+
+
+    }
+    public void clickConnect(View v)
+    {
+        BTScan();
+    }
+
 }
